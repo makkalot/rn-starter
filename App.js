bb30e08dd075431a788b4a73096b0c0cfce36c2a
makkalot <makkalot@gmail.com>
2023-04-10 10:33:28 +0300

    image screen

--- App.js
+++ App.js
@@ -3,12 +3,14 @@ import { createStackNavigator } from "react-navigation-stack";
 import GettingStartedScreen from "./src/screens/GettingStartedScreen";
 import HomeScreen from "./src/screens/HomeScreen";
 import ListScreen from "./src/screens/ListScreen";
+import ImageScreen from "./src/screens/ImageScreen";
 
 const navigator = createStackNavigator(
   {
     GettingStarted: GettingStartedScreen,
     List: ListScreen,
-    Home: HomeScreen
+    Home: HomeScreen,
+    ImageScreen: ImageScreen
   },
   {
     initialRouteName: "Home",
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -3,16 +3,30 @@ import { Text, StyleSheet, View, Button, TouchableOpacity } from "react-native";
 
 const HomeScreen = ({ navigation }) => {
   return (
-    <View>
+    <View style={{ flexDirection: 'column' }}>
       <Text style={styles.text}>HomeScreen</Text>
-      <Button 
-        onPress={() => navigation.navigate('GettingStarted')}
-        title="Go to Components Demo"
-      />
-      <Button 
-        onPress={() => navigation.navigate('List')}
-        title="Go to List Demo"
-      />
+      <View style={styles.buttons}>
+        <Button 
+          onPress={() => navigation.navigate('GettingStarted')}
+          title="Go to Components Demo"
+        />
+      </View>
+
+      <View style={styles.buttons}>
+        <Button 
+          style={styles.buttons}
+          onPress={() => navigation.navigate('List')}
+          title="Go to List Demo"
+        />
+      </View> 
+      <View style={styles.buttons}>
+        <Button 
+          style={styles.buttons}
+          onPress={() => navigation.navigate('ImageScreen')}
+          title="Go to Images"
+        />
+      </View> 
+
     </View>
 
   );
@@ -22,6 +36,9 @@ const styles = StyleSheet.create({
   text: {
     fontSize: 30,
   },
+  buttons: {
+    marginTop: 10
+  }
 });
 
 export default HomeScreen;
